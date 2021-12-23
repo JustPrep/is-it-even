c8b412cd28a204b9a0f5a1c4f904198382374764
JustPrep <78797951+JustPrep@users.noreply.github.com>
2021-12-23 11:32:51 +0400

    made a better solution to it
    yes

--- evenNumberDetector.js
+++ evenNumberDetector.js
@@ -1,5 +1,6 @@
 var number;
 function isEven(number){return number %2;}
+var even_num = 0; var add = 1;
 //i found this code on stack overflow and decided to follow along, also 1 means odd and 0 means even, it describes the difference left until a multiple of 2, and if it's 0 then it's a multiple of 2.
 console.log("1 = " + isEven(1));
 console.log("2 = " + isEven(2));
@@ -13,5 +14,6 @@ console.log("9 = " + isEven(9));
 console.log("10 = " + isEven(10));
 //put your code above this comment to make glory in JavaScript happen in the terminal (i use visual studio code for this coding so i use the node.js terminal to debug my JavaScript code.) 
 if (number/2 == Int32Array){
-    even_num++
+    even_num+= add;
 }
+console.log("Currently, the amount of even numbers by all the pull requests or if natively is " + even_num)
